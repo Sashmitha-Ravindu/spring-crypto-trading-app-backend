9584348e2eb6e135b98370f5b95c838694d8998a
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2025-06-17 17:14:20 +0530

    Initial commit

---
+++ src/main/java/com/sashmitha/springcryptotradingapp/SpringCryptoTradingAppApplication.java
@@ -0,0 +1,13 @@
+package com.sashmitha.springcryptotradingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCryptoTradingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCryptoTradingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sashmitha/springcryptotradingapp/SpringCryptoTradingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sashmitha.springcryptotradingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCryptoTradingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
